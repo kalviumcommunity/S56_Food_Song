63864975d9b22abfd9739e2a3f000caf267f75c3
Joyce Ubale <joyceubale@Joyces-MacBook-Air.local>
2024-02-29 14:37:47 +0530

    React app

---
+++ Client/vite-project/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <div>
+      <nav>
+        <h1>Bollywood Foodie Tunes</h1>
+        <input type="text" placeholder='search song'/>
+        <ul>
+          <li>Home</li>
+          <li>Music</li>
+          <li>About</li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
+
+export default App
