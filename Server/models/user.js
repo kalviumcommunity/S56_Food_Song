898d603ccd6f2a028ff3e165408a6b7fb1d34016
Joyce Ubale <joyceubale@Joyces-MacBook-Air.local>
2024-03-11 14:53:57 +0530

    schema updated

--- Server/models/user.js
+++ Server/models/user.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const UserSchema = new mongoose.Schema({
     food: String,
     lyrics: String,
-    song: String
+    song: String,
+    img: String
 })
 const UserModel = mongoose.model("food", UserSchema)
 module.exports = UserModel
